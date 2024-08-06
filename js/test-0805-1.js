d33d0dbeaab878879a74bb47182a45e4aa9e427f
soojihan <hdocarry@gmail.com>
2024-08-06 15:16:01 +0900

    feat:dl, jq 재연습

---
+++ js/test-0805-1.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('dl dt').on('click',function(){
+        $('+dd', this).slideDown(500).siblings('dd').slideUp(500);
+    })
+    $('dl dd').on('click',function(){
+        $(this).slideUp(500);
+    })
+})
