d4c407e4af79f86dd2748e691ff9e2913e3a06fc
soojihan <hdocarry@gmail.com>
2024-08-22 13:14:41 +0900

    fix: 자바스크립트 추가

--- js/test-0819-2.js
+++ js/test-0819-2.js
@@ -1,3 +1,4 @@
+// 제이쿼리
 $(function(){
     $('#open-btn').on('click', function(){
         $('#popup-box').addClass('show');
@@ -6,3 +7,14 @@ $(function(){
         $('#popup-box').removeClass('show');
     });
 });
+
+// 자바스크립트
+function goToPage (){
+    let name = prompt('성함을 입력해주세요');
+
+    // 옛날 방법
+    // alert('안녕하세요' + name + '님 환영합니다.');
+
+    // 요즘 방법
+    alert(`안녕하세요 ${name}님 환영합니다!`)
+}
