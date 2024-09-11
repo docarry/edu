a91db76889f200026e08aeaeeb5324cd625b165e
soojihan <hdocarry@gmail.com>
2024-09-11 11:48:52 +0900

    feat: toggleBtn 연습

---
+++ js/test-0828-1.js
@@ -0,0 +1,14 @@
+// let btnOn = true;
+
+// function moveOn(){
+//     if(btnOn === true){
+//         document.querySelector('.toggleBtnOn').classList.add('moveBtn');
+//         btnOn = false;
+//     }else{
+//         document.querySelector('.toggleBtnOn').classList.remove('moveBtn');
+//         btnOn = true;
+//     }
+// }
+
+// document.querySelector('.toggleBtnOn').addEventListener('click', moveOn);
+
